4d63621768462a71273c411a9331a4290f530aa6
zhoubin911 <zhobinghahvarehchi@gmail.com>
2025-10-02 00:21:26 +0330

    Delete lenovo.py

--- lenovo.py
+++
@@ -1,6 +0,0 @@
-from laptop import Laptop
-
-class Lenovo(Laptop):
-    def __init__(self, name, price, voltage, cpu, ram, serial2):
-        super().__init__(name, price, voltage, cpu, ram)
-        self.serial2 = serial2
