2a41a18002f3fc214daaec0463a2c978e058f755
zhoubin911 <zhobinghahvarehchi@gmail.com>
2025-10-02 00:20:45 +0330

    Delete asus.py

--- asus.py
+++
@@ -1,6 +0,0 @@
-from laptop import Laptop
-
-class Asus(Laptop):
-    def __init__(self, name, price, voltage, cpu, ram, serial1):
-        super().__init__(name, price, voltage, cpu, ram)
-        self.serial1 = serial1
