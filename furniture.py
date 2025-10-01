0a9e4a58c0e3d6934d7b1278639c74ff1052133f
zhoubin911 <zhobinghahvarehchi@gmail.com>
2025-10-02 00:21:03 +0330

    Delete furniture.py

--- furniture.py
+++
@@ -1,6 +0,0 @@
-from non_electrical import Non_Electrical
-
-class Furniture(Non_Electrical):
-    def __init__(self, name, price, weight, capacity):
-        super().__init__(name, price, weight)
-        self.capacity = capacity
